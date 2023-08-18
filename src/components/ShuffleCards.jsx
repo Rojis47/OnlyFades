8c88bb98a6228eb5fc200a6e577b64691acb91b9
Rogelio Servin <manchaster1509@gmail.com>
2023-08-17 19:21:10 -0500

    up the threshhold

--- src/components/ShuffleCards.jsx
+++ src/components/ShuffleCards.jsx
@@ -9,7 +9,7 @@ import { useEffect } from 'react';
 const ShuffleCards = ({ setShowModal, setSelectedBarber }) => {
   const controls = useAnimation();
   const { ref, inView } = useInView({
-    threshold: 0.1,
+    threshold: 0.5,
   });
 
   useEffect(() => {
